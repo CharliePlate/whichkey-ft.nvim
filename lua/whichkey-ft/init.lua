96ebad0d573af68717defeac3b17f64d4438f7c5
Charlie Plate <charlieplate@Macbook.local>
2022-10-14 11:21:12 -0400

    reset to defualt

--- lua/whichkey-ft/init.lua
+++ lua/whichkey-ft/init.lua
@@ -3,35 +3,7 @@ local whichkey = require("which-key")
 
 M.config = {
 	enable = true,
-	filetypes = {
-		go = {
-			L = {
-				name = "Go",
-				i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
-				t = { "<cmd>GoMod tidy<cr>", "Tidy" },
-				a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
-				A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
-				E = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
-				g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
-				f = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
-				e = { "<cmd>GoIfErr<cr>", "Go If Err" },
-				c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
-			},
-		},
-		typescript = {
-			t = {
-				name = "TypeScript",
-				a = { "<cmd>TSLspOrganize<cr>", "Organize Imports" },
-				A = { "<cmd>TSLspOrganize<cr>", "Organize Imports" },
-				f = { "<cmd>TSLspFixCurrent<cr>", "Fix Current" },
-				F = { "<cmd>TSLspFixCurrent<cr>", "Fix Current" },
-				r = { "<cmd>TSLspRenameFile<cr>", "Rename File" },
-				R = { "<cmd>TSLspRenameFile<cr>", "Rename File" },
-				i = { "<cmd>TSLspImportAll<cr>", "Import All" },
-				I = { "<cmd>TSLspImportAll<cr>", "Import All" },
-			},
-		},
-	},
+	filetypes = {},
 	whichkey_opts = {
 		mode = "n",
 		prefix = "<leader>",
@@ -69,5 +41,3 @@ M.setup = function(config)
 end
 
 return M
-
--- M.setup()
